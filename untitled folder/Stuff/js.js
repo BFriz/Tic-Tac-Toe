0576ef9985ff2eb47a3d81e48faeb36ce2a417f4
Ben Frisby <bfrisb@gmail.com>
2015-05-12 14:07:48 +0100

    have the box up, and listening to the clicks

--- untitled folder/Stuff/js.js
+++ untitled folder/Stuff/js.js
@@ -1,5 +1,15 @@
+var button = $('button');
+
+
 $(document).ready(function(){
-  $('.buttons').on("click", function(){
-    console.log("I'd wear you")
-  })
+  for(var i = 0; i < 1; i++){
+    $('.box').on("click", function(){
+      if ('.box' % 2 === 0){
+        ($(this).append('x'))
+       // console.log($(this).attr('name'))
+      }else {
+        ($(this).append('o'))
+      }
+    })
+  }
 })
