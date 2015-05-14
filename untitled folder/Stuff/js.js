ceae162f4d7b5cc72c703c6bf02ee834d0a92af6
Ben Frisby <bfrisb@gmail.com>
2015-05-14 16:14:55 +0100

    working on setting up a winning page

--- untitled folder/Stuff/js.js
+++ untitled folder/Stuff/js.js
@@ -1,30 +1,47 @@
   var count = 0;
-  var win = '';
   var result = ['0','1','2','3','4','5','6','7','8'];
   var winCombo = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8], [2,4,6]]
 
 $('.box').on("click", getMoves);
-
 function getMoves() {
-  if ((count % 2) === 0) {
-      $(this).html('X');
-        currentPlace = parseInt(this.id);
-          result[currentPlace] = 'X';
-  } else {
-      $(this).html('O');
-        currentPlace = parseInt(this.id);
-          result[currentPlace] = 'O';
+  if ($(this).html() === ''){
+    if ((count % 2) === 0) {
+        $(this).html('<img class="pic1 animated rotateIn" src="pics/beds.png"></img>');
+          currentPlace = parseInt(this.id);
+            result[currentPlace] = 'X';
+    } else {
+        $(this).html('<img class="pic2 animated rollIn"src="pics/beer.png"></img>');
+          currentPlace = parseInt(this.id);
+            result[currentPlace] = 'O';
     }   
-  count++;
-  checkWin();
+    count++;
+    checkWin();
+  }
 }
 function checkWin() {
   $.each(winCombo, function(index, value){
     if (result[value[0]]==result[value[1]] && result[value[1]]==result[value[2]]){
-      prompt('hope');
-    } else {
-      console.log('fail');
-
-    }  
+      $('.box').off();
+        if (result[value[0]] == 'X'){
+            
+        } else {
+            prompt('beer')
+        }
+        debugger;
+    } else if (count === 9){
+      console.log('ty');
+    }  else {
+      console.log('dawg')
+    }
   })
-}  
+} 
+$('.again').on("click", function(){
+  count = 0;
+  result = ['0','1','2','3','4','5','6','7','8'];
+  var buttonArray = $('.box');
+    $.each(buttonArray, function(index, butt){
+      if (butt.children[0]) {
+        butt.children[0].remove();
+      }
+    })
+}) 
